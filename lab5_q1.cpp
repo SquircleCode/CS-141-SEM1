1af0282165b18d33fc17d87fb0fe59cc3ffc2e04
Sai Krishna I <saikrishna.i@cclab203.niser.ac.in>
2018-09-02 18:23:00 +0530

    Uploader lab5_q1 using a script

---
+++ lab5_q1.cpp
@@ -0,0 +1,32 @@
+/*
+* Auto-Generated File
+* Author 
+	Name	: SAI KRISHNA I
+	Roll No : 1811131
+*/
+
+// Importing Libraries
+#include <iostream>
+
+using namespace std;
+
+// Main Method
+int main(){
+	// To-Do
+	// Input Two numbers
+	int a,b,g;
+	
+	cout << "Enter two numbers : ";
+	cin >> a >> b;
+	//find greater number 
+	if(a>b)
+		g=a;
+	else
+		g=b;
+	//prints the number
+	cout << "The greater number is " << g << endl;
+		
+	return 0;
+}
+
+
