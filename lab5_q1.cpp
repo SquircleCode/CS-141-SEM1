6e12a8e1ffaef894ca1a402ca7865976374c0b78
Sai Krishna I <saikrishna.i@cclab108.niser.ac.in>
2018-09-05 15:05:12 +0530

    Uploaded using cpp_file_manager. Created by SaiKrishna

--- lab5_q1.cpp
+++ lab5_q1.cpp
@@ -1,8 +1,6 @@
 /*
 * Auto-Generated File
-* Author 
-	Name	: SAI KRISHNA I
-	Roll No : 1811131
+* Created Using cpp_file_manager
 */
 
 // Importing Libraries
@@ -13,19 +11,7 @@ using namespace std;
 // Main Method
 int main(){
 	// To-Do
-	// Input Two numbers
-	int a,b,g;
-	
-	cout << "Enter two numbers : ";
-	cin >> a >> b;
-	//find greater number 
-	if(a>b)
-		g=a;
-	else
-		g=b;
-	//prints the number
-	cout << "The greater number is " << g << endl;
-		
+
 	return 0;
 }
 
