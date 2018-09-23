c7bc3e2659f1a9676e6dd98646cd9699bd1f84bf
SquircleCode <42237261+SquircleCode@users.noreply.github.com>
2018-09-23 19:40:05 -0400

    empty files

---
+++ lab6_q2.cpp
@@ -0,0 +1,20 @@
+/*
+* Auto-Generated File
+* Created Using cpp_file_manager
+*/
+
+// Importing Libraries
+#include <iostream>
+
+using namespace std;
+
+
+	
+// Main Method
+int main(){
+	// To-Do
+	
+	return 0;
+}
+
+
