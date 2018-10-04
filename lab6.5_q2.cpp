7ef10e459c93ec44d3a75fa94c1aca9fe897a249
SquircleCode <42237261+SquircleCode@users.noreply.github.com>
2018-10-04 11:50:03 +0530

    pseudo code only

---
+++ lab6.5_q2.cpp
@@ -0,0 +1,37 @@
+//import libraries
+#include <iostream>
+/*
+ * Question 2 (Functions): You are ab salesperson of Bata shoe company, where each shoe is Rs 225. Company gave you three options for the salary.
+1. Just a salary of Rs 600 per week;
+2. A salary of Rs7.00 per hour plus a 10% commission on sales; (Assume you work 40 hours a week)
+3. No salary, but 20% commissions and Rs20 for each pair of shoes sold
+Write a program to help decide the best choice of compensation.
+You ask the user for input (Write a function for this).
+Then write other functions for each of the options.
+Write a driver program to use the 4 functions and decide the best option for yourself.
+(Of course, the weekly sales - the number of shoes sold per week has to be given by the user)
+ */
+
+// input function
+	//question out
+	//answer in
+	//return value - weekly sales - number of shoes sold per week
+
+// fixed salary
+	//just stays 600 no matter what
+
+// salary + commission
+	// salary (sal) = 7 rs/hr
+	// working hrs (wh) = 40
+	// tot_sal = sal x wh
+	// comm = 10%
+	// sales = 225 * weekly sales
+	// tot_comm = comm * sales
+	// money earned = tot_sal + tot_comm
+
+// no salary, only commission
+	//sales = 225 * weekly sales
+	//tot_comm = 0.2 * sales
+	//
+
+// main function
