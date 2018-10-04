d49e108967aa7bc93b3fcd9601ba699a9e861ec1
SquircleCode <42237261+SquircleCode@users.noreply.github.com>
2018-10-04 17:08:07 +0530

    completed

--- lab6.5_q2.cpp
+++ lab6.5_q2.cpp
@@ -21,34 +21,54 @@ Write a driver program to use the 4 functions and decide the best option for you
  */
 
 // input function
+int inputFunc(){
+	//variables
+	int weekly_sales;
 	//question out
+	cout << "Enter the weekly sales : ";
 	//answer in
+	cin >> weekly_sales;
 	//return value - weekly sales ( number of shoes sold per week )
-
+	return weekly_sales;	
+	}
 // fixed salary
+float fixedSalary(){
 	//just stays 600 no matter what
+	return 600.0;	
+	}
 
 // salary + commission
+float variableSalary(float sales){
 	//input - sales
 	// salary (sal) = 7 rs/hr
+	float sal = 7;
 	// working hrs (wh) = 40
+	int wh = 40;
 	// tot_sal = sal x wh
+	float tot_sal = sal*wh;	
 	// comm = 10%
+	float comm = 0.1;
 	// tot_comm = comm * sales
+	float tot_comm = comm * sales;
 	// money_earned = tot_sal + tot_comm
+	float money_earned = tot_sal + tot_comm;
 	//output - money_earned
- 
-// no salary, only commission
-	// input - weekly_sales, sales
-	//tot_ValComm = 0.2 * sales
-	//tot_NumComm = 20 * weekly_sales
-	//money_earned = tot_comm + tot_NumComm
-	// output - money_earned
+	return money_earned;
+ 	}
 
-// largest number finder
-	// input - 3 numbers
-	// find largest one
-	// output - largest numberof the 3
+// no salary, only commission
+float noSalary (int weekly_sales , float sales){
+	// input - weekly_sales, sales	
+	// ValComm = 0.2 * sales
+	float valComm = 0.2 * sales;
+	// NumComm = 20 * weekly_sales
+	float numComm = 20* weekly_sales; 
+	// money_earned = valComm + numComm
+	float money_earned = valComm + numComm;
+	// output - money_earned
+	return money_earned;
+	}
+
 
 // main function
 int main(){
@@ -56,8 +76,16 @@ int main(){
 	// Variables
 	// weekly_sales, sales
 	// fixedSal, varSal, noSal
-	// store the function values in the variables
+	int weekly_sales;
+	float sales, fixedSal, varSal, noSal;
+	// store the function values in the variables, Calculate sales
+	weekly_sales = inputFunc();
+	sales = 225.0 * weekly_sales;
+	fixedSal = fixedSalary();
+	varSal = variableSalary(sales);
+	noSal = noSalary(weekly_sales,sales);	
 	// compare the values, find largest
+	cout << "The salaries according the 3 schemes are\n1st Scheme (fixed salary) = " << fixedSal <<"\n2nd Scheme (Salary + Commission) = "<<varSal<<"\n3rd Scheme (Only Commission) = "<<noSal<<endl;
 	// output - the largest one and the best case
 	return 0;
 }
