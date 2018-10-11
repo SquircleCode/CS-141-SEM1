9f709c2a84e34109d6e7e450e665540080ae73d4
SquircleCode <42237261+SquircleCode@users.noreply.github.com>
2018-10-11 21:32:00 +0530

    not for evaluation

---
+++ lab8_q1.cpp
@@ -0,0 +1,68 @@
+/*
+* Auto-Generated File
+* Created Using cpp_file_manager
+*/
+
+// Importing Libraries
+#include <iostream>
+
+using namespace std;
+
+int arraySum (int num[], int n){
+	//calculate the sum	
+	int sum = 0;
+	for (int i = 0;i<n;i++){
+		sum +=num[i];	
+	}
+	return sum;
+} 
+
+int arrayInp (){
+	//variables
+	string inpLineString;
+	//user input
+	cout << "Enter a few numbers with spaces in a line";	
+	// stores the line in a string	
+	getline(cin,inpLineString);
+	// points to the first character in the string
+	char *inpLine = &inpLineString.at(0);
+	// creating reference integer point
+	int  ref = 0;
+	int *refPtr = &ref;
+	bool b = true; // some test condition
+	//j- index for blank spaces, i is the index for the char in the string	
+	int i =0;
+	int j =1; 
+	
+	while(*(inpLine+i) !='\0'){ 
+		while(*(inpLine+i) !=' '&& i<=10){
+		 *(refPtr + j)= (*(refPtr + j-1)*10) + (int) *(inpLine+i);
+		i++;
+		cout <<"i = " <<i <<"\t j = "<< j <<"\t value = "<<*(refPtr) << endl;
+		}
+		cout <<"* i = " <<i <<"\t j = "<< j <<"\t value = "<<*(refPtr) << endl;
+		if(*(inpLine+i) ==' '){
+			cout<<"ifff";
+			j++;
+			i++;		
+		}
+		
+	}
+	
+	
+}
+
+
+// Main Function
+int main(){
+	// To-Do
+	// variables
+	// user input
+	// array declaration 	
+	// output
+	//cout << //arraySum(numbers,sizeof(numbers)/sizeof(0))<<endl;
+	arrayInp();
+	return 0;
+}
+
+
