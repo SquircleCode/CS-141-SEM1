ac8e7ba05e296d257294b2a2c11310ddc9ddf17d
SquircleCode <42237261+SquircleCode@users.noreply.github.com>
2018-10-15 05:50:45 -0400

    not tested

--- lab8_q1.cpp
+++ lab8_q1.cpp
@@ -17,52 +17,14 @@ int arraySum (int num[], int n){
 	return sum;
 } 
 
-int arrayInp (){
-	//variables
-	string inpLineString;
-	//user input
-	cout << "Enter a few numbers with spaces in a line";	
-	// stores the line in a string	
-	getline(cin,inpLineString);
-	// points to the first character in the string
-	char *inpLine = &inpLineString.at(0);
-	// creating reference integer point
-	int  ref = 0;
-	int *refPtr = &ref;
-	bool b = true; // some test condition
-	//j- index for blank spaces, i is the index for the char in the string	
-	int i =0;
-	int j =1; 
-	
-	while(*(inpLine+i) !='\0'){ 
-		while(*(inpLine+i) !=' '&& i<=10){
-		 *(refPtr + j)= (*(refPtr + j-1)*10) + (int) *(inpLine+i);
-		i++;
-		cout <<"i = " <<i <<"\t j = "<< j <<"\t value = "<<*(refPtr) << endl;
-		}
-		cout <<"* i = " <<i <<"\t j = "<< j <<"\t value = "<<*(refPtr) << endl;
-		if(*(inpLine+i) ==' '){
-			cout<<"ifff";
-			j++;
-			i++;		
-		}
-		
-	}
-	
-	
-}
 
 
 // Main Function
 int main(){
 	// To-Do
-	// variables
-	// user input
-	// array declaration 	
+	// array declaration
+	int numbers [] ={1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1}; 	
 	// output
-	//cout << //arraySum(numbers,sizeof(numbers)/sizeof(0))<<endl;
-	arrayInp();
+	cout << arraySum(numbers,sizeof(numbers)/sizeof(0))<<endl;
 	return 0;
 }
-
-
---
+++ lab8_q2.cpp
@@ -0,0 +1,41 @@
+/*
+* Auto-Generated File
+* Created Using cpp_file_manager
+*/
+
+// Importing Libraries
+#include <iostream>
+
+using namespace std;
+
+// sorting function - important- rest follows easily
+int arraySorter [] (int arrayRandom [],int n){
+	bool sorted = false;
+	while(!sorted){
+		for (int i =0; i<n;i++){
+			if (arrayRandom [i] > arrayRandom [i+1] )
+		}
+	}
+}
+// max func
+// min func
+// mode func
+// median func
+// avg func
+
+// Main Function
+int main(){
+	// To-Do
+	// variables for output
+	int n;
+	// array declaration
+	int arrayRandom [] ={12,432,1,23,43,123};
+	int n = sizeof(arrayRandom)/sizeof(0);	
+	// sort the array
+	// number of elemets
+	// max,min,avg,med,mode
+	// output 
+	return 0;
+}
+
+
