f87499f5bca2262181f827553cd4b7b306a52e65
SquircleCode <42237261+SquircleCode@users.noreply.github.com>
2018-10-15 07:44:01 -0400

    mode not complete

--- lab8_q2.cpp
+++ lab8_q2.cpp
@@ -9,33 +9,77 @@
 using namespace std;
 
 // sorting function - important- rest follows easily
-int arraySorter [] (int arrayRandom [],int n){
-	bool sorted = false;
-	while(!sorted){
+// bubble sort
+int *arraySorter (int arrayRandom [],int n){
+	for(int j = 1; j<n;j++){
 		for (int i =0; i<n;i++){
-			if (arrayRandom [i] > arrayRandom [i+1] )
+			if (arrayRandom [i] > arrayRandom [i+1] ){
+					int temp = 	arrayRandom [i];
+					arrayRandom [i] = arrayRandom [i+1] ;
+					arrayRandom [i+1] = temp;			
+			}
 		}
 	}
+	return arrayRandom;
 }
+
+
 // max func
+int arrayMax(int arrayRand[],int n){
+	int *arraySorted = arraySorter(arrayRand,n);
+	return *(arraySorted+n-1);
+}
 // min func
+int arrayMin(int arrayRand[],int n){
+	int *arraySorted = arraySorter(arrayRand,n);
+	return *(arraySorted);
+}
+	
 // mode func
+
+
 // median func
-// avg func
+int arrayMedian(int arrayRand[],int n){
+	int *arraySorted = arraySorter(arrayRand,n);
+	if(n%2==1)
+	return *(arraySorted+(n/2)+1);
+	else
+	return (*(arraySorted+(n/2))+*(arraySorted+(n/2)))/2;
+}
+
+//mean
+int arrayMean (int num[], int n){
+	//calculate the sum	
+	int sum = 0;
+	for (int i = 0;i<n;i++){
+		sum +=num[i];	
+	}
+	return sum/n;
+}
 
 // Main Function
 int main(){
 	// To-Do
 	// variables for output
-	int n;
+	int n,max,min,median,mode,mean;
 	// array declaration
 	int arrayRandom [] ={12,432,1,23,43,123};
-	int n = sizeof(arrayRandom)/sizeof(0);	
-	// sort the array
 	// number of elemets
-	// max,min,avg,med,mode
+	 n = sizeof(arrayRandom)/sizeof(0);	
+	
+	// max
+		max = arrayMax(arrayRandom,n);
+	 
+	// min 
+		min = arrayMin(arrayRandom,n);
+	// mean
+	 	mean = arrayMean(arrayRandom,n);
+	// median 
+		median = arrayMedian(arrayRandom,n);
+	// mode
+
 	// output 
+	cout << "\nMean\t=\t"<<mean<< "\nMedian\t=\t"<<median<<"\nMaximum\t=\t"<<max<<"\nMinimum\t=\t"<<min<<endl;
 	return 0;
 }
-
 
